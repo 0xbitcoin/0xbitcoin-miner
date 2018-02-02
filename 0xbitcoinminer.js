29909888439388cebce5ce58d50405c8fc0c0118
Andy <admazzola@gmail.com>
2018-02-02 01:13:40 -0500

    wont compile now

--- 0xbitcoinminer.js
+++ 0xbitcoinminer.js
@@ -2,6 +2,7 @@
 
 var web3utils =  require('web3-utils');
 
+var solidityHelper = require('./solidity-helper')
 
 var leftpad =  require('leftpad');
 
@@ -9,6 +10,11 @@ var miningDifficulty = 4;
 var challengeNumber = 'aaa';
 
 var tokenContract;
+
+
+// 164892630 '00004fd994cbd58363bdfe0809d1a6d58e1646b56d1bb59154e45d555a6615aa'
+
+
 
 module.exports =  {
 
@@ -59,6 +65,7 @@ module.exports =  {
     async collectDataFromContract()
     {
 
+
       console.log('collecting data from smartcontract');
     //  miningDifficulty = 4;
     //  challengeNumber = 'aaa';
@@ -70,7 +77,23 @@ module.exports =  {
       var chall = await tokenContract.methods.getChallengeNumber().call({} ) ;
       challengeNumber = chall;
 
-      console.log(miningDifficulty,challengeNumber);
+      console.log('difficulty:', miningDifficulty);
+      console.log('challenge number:', challengeNumber)
+
+    },
+
+    async submitNewMinedBlock(nonce,digest_bytes)
+    {
+       console.log('Submitting block for reward')
+       console.log(nonce,digest_bytes)
+
+
+
+      tokenContract.methods.mint(nonce,digest_bytes).send({}, function(error,result){
+         console.log(error,result)
+       } ) ;
+      //  console.log('success',success)
+
 
     },
 
@@ -102,10 +125,10 @@ module.exports =  {
                if ( zeroesCount >= difficulty )
                {
 
-                 console.log('found a good one')
 
-                console.log(nonce,digest)
-              //   this.mining=false;
+
+                this.submitNewMinedBlock(nonce,solidityHelper.stringToSolidityBytes32(digest));
+
                }
 
 
--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var Web3 = require('web3-eth')
 const Miner = require("./0xbitcoinminer");
 
 
-var INFURA_ROPSTEN_URL = 'https://ropsten.infura.io/gmXEVo5luMPUGPqg6mhy';
+//var INFURA_ROPSTEN_URL = 'https://ropsten.infura.io/gmXEVo5luMPUGPqg6mhy';
+var LOCAL_GETH = 'http://localhost:8545';
+
 var SMARTCONTRACT_ADDRESS ='0x9ec7567938f19d08d1915c2ab7ed23c743e49e31'
 
-var web3 = new Web3(new Web3.providers.HttpProvider(INFURA_ROPSTEN_URL));
+//var web3 = new Web3(new Web3.providers.HttpProvider(LOCAL_GETH));
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
-var tokenContract = require('./_0xBitcoinToken.json');
 
 if (process.argv.length <= 2) {
 console. log("Please add a difficulty parameter (1-64)");
@@ -18,9 +21,19 @@ process. exit(-1);
 var account_address =  process.argv[2] ;
 
 
-var contract = new web3.Contract(tokenContract.abi, SMARTCONTRACT_ADDRESS, {
+var json = JSON.parse(require('fs').readFileSync('./_0xBitcoinToken.json', 'utf8'));
+console.log(json.abi)
+
+var contract = new web3.Contract(json.abi, SMARTCONTRACT_ADDRESS, {
    from: account_address, // default from address
    gasPrice: '2000000000' // default gas price in wei, 2 gwei in this case
 });
 
-Miner.init( web3 , contract, account_address )
+var diff =   contract.methods.getMiningDifficulty().call({},function(error,result){
+
+  console.log('difficulty:', result, error);
+} ) ;
+
+
+
+//Miner.init( web3 , contract, account_address )
