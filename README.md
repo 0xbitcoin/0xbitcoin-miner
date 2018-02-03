0bd2b261a3b0ca0ae2183fe9421e1f76e2226bee
andy <andy@jonezie.com>
2018-02-03 10:43:00 -0500

    able to send raw tx almost

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## 0xBitcoin Miner
+
+
+ 
--- index.js
+++ index.js
@@ -1,67 +1,59 @@
 var Web3 = require('web3')
 const Miner = require("./0xbitcoinminer");
 
+const Vault = require("./lib/vault");
+
+var NetworkInterface = require("./lib/network-interface");
+
+var ContractInterface = require("./contracts/DeployedContractInfo")
 
 var INFURA_ROPSTEN_URL = 'https://ropsten.infura.io/gmXEVo5luMPUGPqg6mhy';
-var SMARTCONTRACT_ADDRESS ='0x9ec7567938f19d08d1915c2ab7ed23c743e49e31'
 
-//var web3 = new Web3(new Web3.providers.HttpProvider(INFURA_ROPSTEN_URL));
+var web3 = new Web3(new Web3.providers.HttpProvider(INFURA_ROPSTEN_URL));
 
-var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+
+//var web3 = new Web3();
+//web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
+
+
+//send tx
+// https://gist.github.com/raineorshine/c8b30db96d7532e15f85fcfe72ac719c
+// providers https://github.com/MetaMask/provider-engine
+
+
+// Build a wallet vault ??
+
+
+
+// the address that will send the test transaction
+const addressFrom = Vault.getAccount().public_address // '0x1889EF49cDBaad420EB4D6f04066CA4093088Bbd'
+const privKey = Vault.getAccount().private_key
+
+console.log(addressFrom)
+
+// the destination address
+const smartContractAddress = ContractInterface.contracts._0xbitcointoken.blockchain_address;
+
+
+
+
+ var success = NetworkInterface.submitMiningSolution(web3,addressFrom, Vault)
+
 
 
 var tokenContract = require('./_0xBitcoinToken.json');
 
-if (process.argv.length <= 2) {
-console. log("Please add a difficulty parameter (1-64)");
-process. exit(-1);
-}
-
-var account_address =  process.argv[2] ;
-
-var contract =  web3.eth.contract(tokenContract.abi).at(SMARTCONTRACT_ADDRESS)
-
-Miner.init( web3 , contract, account_address )
-
 /*
-var Web3 = require('web3-eth')
-const Miner = require("./0xbitcoinminer");
-
-
-//var INFURA_ROPSTEN_URL = 'https://ropsten.infura.io/gmXEVo5luMPUGPqg6mhy';
-var LOCAL_GETH = 'http://localhost:8545';
-
-var SMARTCONTRACT_ADDRESS ='0x9ec7567938f19d08d1915c2ab7ed23c743e49e31'
-
-//var web3 = new Web3(new Web3.providers.HttpProvider(LOCAL_GETH));
-var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
-
-
 if (process.argv.length <= 2) {
 console. log("Please add a difficulty parameter (1-64)");
 process. exit(-1);
 }
 
 var account_address =  process.argv[2] ;
-
-
-var json = JSON.parse(require('fs').readFileSync('./_0xBitcoinToken.json', 'utf8'));
-console.log(json.abi)
-
-var contract = new web3.Contract(json.abi, SMARTCONTRACT_ADDRESS, {
-   from: account_address, // default from address
-   gasPrice: '2000000000' // default gas price in wei, 2 gwei in this case
-});
-
-var diff =   contract.methods.getMiningDifficulty().call({},function(error,result){
-
-  console.log('difficulty:', result, error);
-} ) ;
-
-
-
-//Miner.init( web3 , contract, account_address )
-
 */
+
+var contract =  web3.eth.contract(tokenContract.abi).at(smartContractAddress)
+
+//  Miner.init( web3 , contract, Vault.getAccount().public_address )
