7ad7b07e23e494972c3ce615565de8f49d0efb81
0xbitcoin <36060731+0xbitcoin@users.noreply.github.com>
2018-02-18 17:30:02 -0500

    Update gpuminer.cc

--- lib/gpuminer.cc
+++ lib/gpuminer.cc
@@ -3,6 +3,23 @@
 //build with
 //    node-gyp configure build --target=v8.9.4
 
+/* 
+
+This c++ addon will be provided uint256 numbers: X, Y and T 
+Where X is an ethereum address
+Y is the 'challenge number'
+T is the target 
+
+This c++ addon will continuously run a loop in which: 
+  1. A random number is generated - called a Nonce
+  2. A result is found of S = Keccak256(nonce,X,Y) 
+  3. If S < T then we will push S to an array 
+  
+  
+From javascript, we will provide X, Y , and T to this addon and we will read back solutions (S) from the array 
+
+
+*/
 
 #include <node.h>
 #include <nan.h>
@@ -27,8 +44,9 @@ using v8::Value;
 
 int difficultyTarget;
 int challengeNumber;
+//string minerEthAddress ; 
 
-
+//int[]  solutions;
 
 void setChallengeNumber(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
@@ -45,24 +63,59 @@ void setDifficultyTarget(const FunctionCallbackInfo<Value>& args) {
   args.GetReturnValue().Set(difficultyTarget);
 }
 
+  
+  
+  //PLEASE FILL ME IN- MOST IMPORTANT FUNCTION 
 void startMining(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+  
+    //start infinite loop
+  
+    while(true) {
+      
+      //generate random number -- nonce 
+    //   int nonce = getRandomNumber();
+      
+      //int result = keccak256(nonce, minerEthAddress, challengeNumber )
+      
+      
+      /*if ( result < difficultyTarget) 
+      {
+      
+            Push the working nonce to an array !! 
+            solutions.push( nonce  ) 
+      }
+      
+      */
+      
+    
+    }
+  
 }
 
+  //not implemented - a Get function so we can read solutions back into javascript 
 void getSolutionsBuffer(const FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = args.GetIsolate();
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
 }
 
+  
+  
+  
+  
+  
 
-
+//what am i doing ?
 
 void setup_kernel ( curandState * state, unsigned long seed )
 {
     int id = threadIdx.x;
     curand_init ( seed, id, 0, &state[id] );
 }
+  
+ 
+
+//what am i doing ?
 
 void addToCount(int N, int *y, curandState* globalState)
 {
@@ -78,6 +131,9 @@ void addToCount(int N, int *y, curandState* globalState)
 }
 
 
+  
+
+//what am i doing ?
 void getRandomNumber()
 {
 
