5e7fc8afffa599bf993498653ee92f4c4d444a82
Andy M <admazzola@gmail.com>
2018-02-22 09:43:05 -0500

    almost have pool mining working with cpp miner addon

--- 0xbitcoinminer-accel.js
+++ 0xbitcoinminer-accel.js
@@ -12,7 +12,7 @@ const GPUMiner = require('./build/Release/gpuminer');
 var tokenContract;
 
 const PRINT_STATS_TIMEOUT = 5000;
-const COLLECT_CONTRACT_DATA_TIMEOUT = 2000;
+const COLLECT_MINING_PARAMS_TIMEOUT = 2000;
 
 
 module.exports =  {
@@ -38,6 +38,11 @@ module.exports =  {
 
     async mine(subsystem_command,subsystem_option)
     {
+
+
+
+      //miningParameters
+
       var eth_account  = this.vault.getAccount();
       console.log('Selected mining account:', eth_account);
 
@@ -50,17 +55,22 @@ module.exports =  {
       var self = this;
       var minerEthAddress = eth_account.address;
 
-      let contractData = {}; //passed around as a reference and edited globally
 
-      this.mineStuff = function(contractData) {
+
+      let miningParameters = {};
+      setInterval(function(){self.collectMiningParameters(minerEthAddress,miningParameters,self.miningStyle)},COLLECT_MINING_PARAMS_TIMEOUT);
+      await self.collectMiningParameters(minerEthAddress, miningParameters,self.miningStyle);
+
+
+      this.mineStuff = function(miningParameters) {
           if (!this.mining) {
-              this.mineCoins(self.web3, contractData, minerEthAddress);
+          //  if (this.mining) {
+              this.mineCoins(self.web3, miningParameters,  minerEthAddress);
           }
       }
 
-      await self.collectDataFromContract(contractData);
-
-      setInterval(() => {self.collectDataFromContract(contractData)}, COLLECT_CONTRACT_DATA_TIMEOUT);
+      //await self.collectDataFromContract(contractData);
+      //setInterval(() => {self.collectDataFromContract(contractData)}, COLLECT_CONTRACT_DATA_TIMEOUT);
 
       this.miningLogger.appendToStandardLog("Begin mining for " + minerEthAddress + " @ gasprice " + this.vault.getGasPriceGwei());
 
@@ -72,7 +82,7 @@ module.exports =  {
 
       // let's mine!
       this.mining = false;
-      this.mineStuff(contractData);
+      this.mineStuff(miningParameters);
     },
 
     setMiningStyle(style)
@@ -80,7 +90,64 @@ module.exports =  {
         this.miningStyle = style;
     },
 
-    async collectDataFromContract(contractData)
+
+    async collectMiningParameters(minerEthAddress,miningParameters,miningStyle)
+    {
+
+      let bResume = false;
+
+      if(miningStyle === "pool")
+      {
+
+        var parameters = await this.networkInterface.collectMiningParameters(minerEthAddress);
+
+
+      }else{
+
+        var parameters = await this.networkInterface.collectMiningParameters();
+
+      }
+
+      //console.log('collected mining params ', parameters)
+      miningParameters.miningDifficulty = parameters.miningDifficulty;
+      miningParameters.challengeNumber = parameters.challengeNumber;
+      miningParameters.miningTarget = parameters.miningTarget;
+      miningParameters.poolEthAddress = parameters.poolEthAddress;
+
+      //give data to the c++ addon
+
+
+      this.updateCPUAddonParameters(miningParameters)
+    },
+
+    async updateCPUAddonParameters(miningParameters){
+
+      var challengeNumber = miningParameters.challengeNumber
+        var miningTarget = miningParameters.miningTarget
+        var miningDifficulty = miningParameters.miningDifficulty
+
+              console.log("New challenge number: " + challengeNumber);
+              CPUMiner.setChallengeNumber(challengeNumber);
+              bResume = true;
+
+               console.log("New mining target: 0x" + miningTarget.toString(16));
+               CPUMiner.setDifficultyTarget("0x" + miningTarget.toString(16));
+
+               console.log("New difficulty: " + miningDifficulty);
+
+               /*
+               if (bResume && !this.mining) {
+                   console.log("Resuming mining operations with new challenge");
+                   this.mineStuff(contractData);
+               }
+               */
+
+    },
+
+    ///refactor
+  /*
+
+   async collectDataFromContract(contractData)
     {
         try {
             const miningDifficultyString = await tokenContract.methods.getMiningDifficulty().call();
@@ -121,33 +188,56 @@ module.exports =  {
         return contractData;
     },
 
-    async submitNewMinedBlock(addressFrom, solution_number, digest_bytes, challenge_number)
+    */
+
+    async submitNewMinedBlock( addressFrom, minerEthAddress, solution_number,digest_bytes,challenge_number, target, difficulty)
+
+    //async submitNewMinedBlock(addressFrom, solution_number, digest_bytes, challenge_number)
     {
         this.miningLogger.appendToStandardLog("Giving mined solution to network interface " + challenge_number);
 
         this.networkInterface.queueMiningSolution(addressFrom, solution_number , digest_bytes , challenge_number)
     },
 
-    mineCoins(web3, contractData , minerEthAddress)
+    // contractData , -> miningParameters
+    mineCoins(web3, miningParameters, minerEthAddress)
     {
-        CPUMiner.setMinerAddress(minerEthAddress);
+
+
+
+      var target = miningParameters.miningTarget;
+      var difficulty = miningParameters.miningDifficulty;
+
+
+      var addressFrom;
+
+      if( this.miningStyle == "pool" ){
+          addressFrom = miningParameters.poolEthAddress;
+      }else{
+          addressFrom = minerEthAddress;
+      }
+
+
+        CPUMiner.setMinerAddress(addressFrom);
 
         var self = this;
 
         const verifyAndSubmit = (solution_number) => {
-            const challenge_number = contractData.challengeNumber;
+            const challenge_number = miningParameters.challengeNumber;
             const digest = web3utils.sha3(challenge_number + minerEthAddress.substring(2) + solution_number.substring(2));
             const digestBigNumber = web3utils.toBN(digest);
-            if (digestBigNumber.lte(contractData.miningTarget)) {
+            if (digestBigNumber.lte(miningParameters.miningTarget)) {
                 console.log('Submit mined solution for challenge ', challenge_number);
-                self.submitNewMinedBlock(minerEthAddress, solution_number, digest, challenge_number);
+              //  self.submitNewMinedBlock(minerEthAddress, solution_number, digest, challenge_number);
+                  self.submitNewMinedBlock( addressFrom, minerEthAddress, solution_number,digest,challenge_number, target, difficulty)
+
             } else {
                 console.error("Verification failed!\n",
                 "challenge: ", challenge_number, "\n",
                 "address: ", minerEthAddress, "\n",
                 "solution: ", solution_number, "\n",
                 "digest: ", digestBigNumber, "\n",
-                "target: ", contractData.miningTarget);
+                "target: ", target);
             }
         }
 
